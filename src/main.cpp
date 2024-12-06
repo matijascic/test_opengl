5465fc34aecfc458d5d2076ac5e78d8daa913603
Hugo Matijascic <hugo.matijascic@polymtl.ca>
2024-12-06 14:39:52 -0500

    setup

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdio>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+const int width = 2000;
+const int height = 1200;
+
+
+int main()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "CAD2UT Testbench", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, width, height);
+
+	bool guiHovered = false;
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
